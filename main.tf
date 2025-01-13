7eca277ed24435389a128a032a059b5313f46306
Dhruvik Mevada <dhruvik333@gmail.com>
2025-01-13 18:34:54 -0500

    Fix main.yml and format terraform files

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "ubuntu_vm" {
   tags = {
     Name = var.instance_name
   }
-  subnet_id       = aws_subnet.terra-vpc-subnet.id
-  vpc_security_group_ids = [ aws_security_group.terra-sec-group.id ]
-  key_name        = var.key_name
-  user_data       = file("scripts/install_apache.sh")
+  subnet_id              = aws_subnet.terra-vpc-subnet.id
+  vpc_security_group_ids = [aws_security_group.terra-sec-group.id]
+  key_name               = var.key_name
+  user_data              = file("scripts/install_apache.sh")
 }
